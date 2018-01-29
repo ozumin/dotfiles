61104ddc4294d9511fac4059b2a9caf920164a1f
Mizuo Nagayama <mizuo.n@asagi.waseda.jp>
2018-01-29 21:46:42 +0900

    second commit

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zprezto ~/.zprezto
+ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zlogin ~/.zlogin
+ln -sf ~/dotfiles/.zlogout ~/.zlogout
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
