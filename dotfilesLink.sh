23b75ad81917ab5d61a563e6580df017dfe012e0
Mizuo Nagayama <mizuo.n@asagi.waseda.jp>
2018-03-24 23:36:41 +0900

    fixed link

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-#ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
-#ln -sf ~/dotfiles/.zshrc ~/.zshrc
-#ln -sf ~/dotfiles/.zlogin ~/.zlogin
-#ln -sf ~/dotfiles/.zlogout ~/.zlogout
-#ln -sf ~/dotfiles/.zprofile ~/.zprofile
-#ln -sf ~/dotfiles/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zlogin ~/.zlogin
+ln -sf ~/dotfiles/.zlogout ~/.zlogout
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
 if [ ! -L ~/.zprezto -a -d ~/dotfiles/prezto ]; then
     ln -s ~/dotfiles/prezto ~/.zprezto
 fi
